5498ee47ca841dcc8d4438c57743ff5df9247581
Nick C Volpe <ncvolpe@ieee.org>
2025-03-24 23:34:22 -0400

    additional experimentation

--- Esp32PersonalTinkering/Platformio Projects/Rtos First Try/src/main.cpp
+++ Esp32PersonalTinkering/Platformio Projects/Rtos First Try/src/main.cpp
@@ -9,6 +9,7 @@ void IRAM_ATTR onTimer(){
   portENTER_CRITICAL_ISR(&timerMux);
 Serial.println(String("onTimer() ") + String(millis()));
   portEXIT_CRITICAL_ISR(&timerMux);
+  // Although he talked about this weird port stuff, he didn't use it in his code. why?
 }
 
 void setup(){
@@ -16,11 +17,14 @@ void setup(){
   Serial.println("start timer");
   timer = timerBegin(0, 80, true);// timer 0, MWDT clock period = 12.5ns * TIMGn_Tx_WDT_CLK_PRESCALE -> 12.5 ns * 80 - > 1000 ns = 1us
   timerAttachInterrupt(timer, &onTimer, true); // edge (not level) triggered
-  timerAlarmWrite(timer, 1000000, true);
+  timerAlarmWrite(timer, 1000000, true); // 1000000 * 1 us = 1s, autoreload true
+  //so, theoretically, every 1 second, the interrupt function is called? Wow.
   timerAlarmEnable(timer);
 
 }
 
 void loop(){
   vTaskDelay(portMAX_DELAY);
+  Serial.println("meow"); // Doesn't do anything
+  delay(500); // Doesn't do anything
 }
