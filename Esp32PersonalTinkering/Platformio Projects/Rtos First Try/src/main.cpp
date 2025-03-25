32f3495321fed9f58c6cfbcb3337cb70807129a5
Nick C Volpe <ncvolpe@ieee.org>
2025-03-24 23:15:18 -0400

    Adding notes on timing for future clarity

--- Esp32PersonalTinkering/Platformio Projects/Rtos First Try/src/main.cpp
+++ Esp32PersonalTinkering/Platformio Projects/Rtos First Try/src/main.cpp
@@ -13,7 +13,7 @@ Serial.println(String("onTimer() ") + String(millis()));
 void setup(){
   Serial.begin(115200);
   Serial.println("start timer");
-  timer = timerBegin(0, 80, true);
+  timer = timerBegin(0, 80, true);// timer 0, MWDT clock period = 12.5ns * TIMGn_Tx_WDT_CLK_PRESCALE -> 12.5 ns * 80 - > 1000 ns = 1us
   timerAttachInterrupt(timer, &onTimer, true); // edge (not level) triggered
   timerAlarmWrite(timer, 1000000, true);
   timerAlarmEnable(timer);
